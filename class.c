2b05a000ea29bb1369c2de64126fc716f92bb92c
MV2 <maheshchary660@gmail.com>
2025-09-02 19:22:54 +0530

    Add files via upload

---
+++ class.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main() {
+    // int i=0,a,b;
+    // i++; // i=i+1, postfix increment
+    // ++i; // i=i+1, prefix increment
+    // a=i++; // a=i => i=i+1
+    // b=++i; // i=i+1 => b=i
+    // printf("i=%d a=%d b=%d\n", i, a, b);
+    // // try for --i and i--
+
+    
+
+    //ternary operator ?:
+    // int a=5, b=10,i=5, c;
+    // c= a<b?10:20;
+    // a<b?i++:i--;
+    // a<b?printf("a is less than b\n"):printf("b is greater than a\n");
+    // printf("c=%d\n", c);
+    // printf("i=%d\n", i);
+
+
+
+    //bitwise operators
+    // printf("8 & 9 = %d\n", 8 & 9);
+    // printf("8 | 9 = %d\n", 8 | 9);
+    // printf("8 ^ 9 = %d\n", 8 ^ 9);
+    // printf("~8 = %d\n", ~8);
+    // printf("9 << 3 = %d\n", 9 << 3);
+    // printf("8 >> 2 = %d\n", 8 >> 2);
+    // printf("Size of int = %zu bytes\n", sizeof(int));
+
+}
+/**
+ * Unary(++,--,!), binary(+,+=..), ternary(?:)
+ *   2        +           6
+ * operand operator    operand
+ * arithmetic operations
+ *  + -> 2+6
+ * assignment operations
+ * logical operations
+ * relational operations
+ * increment/decrement operations
+ * ternary operations
+ * bitwise operations(&,|,^,>>,<<,~)
+ *
+ *
+ */
